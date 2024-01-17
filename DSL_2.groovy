cb076e240b53b466acb8d8956719905d4201ac7b
iDhruven <35227460+iDhruven@users.noreply.github.com>
2024-01-17 13:17:15 -0800

    Updated pipelineJob to jobDsl

--- DSL_2.groovy
+++ DSL_2.groovy
@@ -2,7 +2,7 @@ def gitRepoURL = 'https://github.com/iDhruven/Jenkins'
 def gitCredentialsId = 'ac98bbd7-38fc-4a1f-8339-ddc65c6716a2'
 
 
-pipelineJob('Superman_Pipeline') {
+jobDsl('Superman_Pipeline') {
     definition {
         cpsScm {
             scm {
