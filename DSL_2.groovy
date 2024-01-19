7879a818647f07e4e5b0ebba2c524afcebba6965
iDhruven <35227460+iDhruven@users.noreply.github.com>
2024-01-19 15:16:03 -0800

    Adding it as a function

--- DSL_2.groovy
+++ DSL_2.groovy
@@ -1,61 +1,43 @@
 def gitRepoURL = 'https://github.com/iDhruven/Jenkins'
 def gitCredentialsId = 'ac98bbd7-38fc-4a1f-8339-ddc65c6716a2'
 
-
-pipelineJob('Superman_Pipeline') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url(gitRepoURL)
-                        credentials(gitCredentialsId)
-                    }
-                    branch('${branchName}')
-                    browser {
-                        githubWeb {
-                            repoUrl(gitRepoURL)
+def generateJob() {
+    pipelineJob('Superman_Pipeline') {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(gitRepoURL)
+                            credentials(gitCredentialsId)
+                        }
+                        branch('${branchName}')
+                        browser {
+                            githubWeb {
+                                repoUrl(gitRepoURL)
+                            }
                         }
                     }
+                    scriptPath('Jenkinsfile')
+                    lightweight(true)
                 }
-                scriptPath('Jenkinsfile')
-                lightweight(true)
-            }
 
-            /*
-            triggers {
-                genericTrigger {
-                    genericVariables {
-                        genericVariable {
-                            key('branchName')
-                            expressionType('JSONPath')
-                            value('')
-                            defaultValue('main')
+                triggers {
+                    genericTrigger {
+                        genericVariables {
+                            genericVariable {
+                                key('branchName')
+                                expressionType('JSONPath')
+                                value('')
+                                defaultValue('main')
+                            }
                         }
-                    }
-                    token('abc123')
-                    printPostContent(true)
-                    printContributedVariables(true)
-                    causeString('Generic Cause')
-                }
-            }
-            */
-        }
-
-        triggers {
-            genericTrigger {
-                genericVariables {
-                    genericVariable {
-                        key('branchName')
-                        expressionType('JSONPath')
-                        value('')
-                        defaultValue('main')
+                        token('abc123')
+                        printPostContent(true)
+                        printContributedVariables(true)
+                        causeString('Generic Cause')
                     }
                 }
-                token('abc123')
-                printPostContent(true)
-                printContributedVariables(true)
-                causeString('Generic Cause')
             }
         }
     }
