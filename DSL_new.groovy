ba584107037265c6ba7b917711ed5bf275de9972
iDhruven <35227460+iDhruven@users.noreply.github.com>
2024-01-22 12:09:52 -0800

    Update DSL_new.groovy

--- DSL_new.groovy
+++ DSL_new.groovy
@@ -1,1 +1,32 @@
+def gitRepoURL = 'https://github.com/iDhruven/Jenkins'
+def gitCredentialsId = 'ac98bbd7-38fc-4a1f-8339-ddc65c6716a2'
 
+def generateJob() {
+    pipelineJob('Superman_Pipeline') {
+        definition {
+            script {
+                git remoteUrl gitRepoURL
+                git credentialsId gitCredentialsId
+                git branch 'main' // or replace with your desired branch name
+                load 'Jenkinsfile'
+            }
+        }
+
+        triggers {
+            genericTrigger {
+                token('abc123')
+                printPostContent(true)
+                printContributedVariables(true)
+                causeString('Generic Cause')
+                genericVariables {
+                    genericVariable {
+                        key('branchName')
+                        expressionType('JSONPath')
+                        value('')
+                        defaultValue('main')
+                    }
+                }
+            }
+        }
+    }
+}
